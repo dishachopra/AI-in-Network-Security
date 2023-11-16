54677c5b30aa6d7104042ae20ee3c25f11bea162
dishachopra <123184552+dishachopra@users.noreply.github.com>
2023-11-16 16:01:54 +0530

    Update net.py

--- networks/net.py
+++ networks/net.py
@@ -11,25 +11,25 @@ image_path = "networks/normalVSattack.png"
 st.image(image_path, caption='Your Image Caption', use_column_width=True)
 image_path1 = "networks/top5attacks.png"
 st.image(image_path1, caption='Your Image Caption', use_column_width=True)
-image_path2 = "/Users/disha/Desktop/networks/noOfrestAttacks.png"
+image_path2 = "networks/noOfrestAttacks.png"
 st.image(image_path2, caption='Your Image Caption', use_column_width=True)
 
 st.subheader("Feature Importance")
 #heading for feature importance
 st.header("Binary Classification")
-image_path4 = "/Users/disha/Desktop/networks/featureImpBin.png"
+image_path4 = "networks/featureImpBin.png"
 st.image(image_path4, caption='Your Image Caption', use_column_width=True)
 
 image_paths = {
-    'Random Forest': '/Users/disha/Desktop/networks/rfBin.png',
-    'Naive Bais': '/Users/disha/Desktop/networks/naiveBin.png',
-    'Logisic Regression': '/Users/disha/Desktop/networks/lrBin.png',
-    'MLP': '/Users/disha/Desktop/networks/mlpBin.png',
-    'K Nearest Neighbors': '/Users/disha/Desktop/networks/knnBin.png',
-    'Variational Autoencoders': '/Users/disha/Desktop/networks/vaeBin.png',
-    'Convolutional Neural Networks': '/Users/disha/Desktop/networks/cnnBin.png',
-    'Isolation Forest': '/Users/disha/Desktop/networks/isolationBin.png',
-    'Adaboost': '/Users/disha/Desktop/networks/AdaBoostBin.png'
+    'Random Forest': 'networks/rfBin.png',
+    'Naive Bais': 'networks/naiveBin.png',
+    'Logisic Regression': 'networks/lrBin.png',
+    'MLP': 'networks/mlpBin.png',
+    'K Nearest Neighbors': 'networks/knnBin.png',
+    'Variational Autoencoders': 'networks/vaeBin.png',
+    'Convolutional Neural Networks': 'networks/cnnBin.png',
+    'Isolation Forest': 'networks/isolationBin.png',
+    'Adaboost': 'networks/AdaBoostBin.png'
     # Add more images as needed
 }
 
@@ -45,17 +45,17 @@ else:
 st.subheader("Feature Importance")
 #heading for feature importance
 st.header("Multiclass Classification")
-image_path5 = "/Users/disha/Desktop/networks/featureImpMulti.png"
+image_path5 = "networks/featureImpMulti.png"
 st.image(image_path5, caption='Your Image Caption', use_column_width=True)
 
 image_paths = {
-    'Random Forest': '/Users/disha/Desktop/networks/rfMulti.png',
-    'Naive Bais': '/Users/disha/Desktop/networks/naiveMulti.png',
-    'Logisic Regression': '/Users/disha/Desktop/networks/lrMulti.png',
-    'MLP': '/Users/disha/Desktop/networks/mlpMulti.png',
-    'K Nearest Neighbors': '/Users/disha/Desktop/networks/knnMulti.png'
+    'Random Forest': 'networks/rfMulti.png',
+    'Naive Bais': 'networks/naiveMulti.png',
+    'Logisic Regression': 'networks/lrMulti.png',
+    'MLP': 'networks/mlpMulti.png',
+    'K Nearest Neighbors': 'networks/knnMulti.png'
     }
-image_path6 = "/Users/disha/Desktop/networks/attacks.png"
+image_path6 = "networks/attacks.png"
 st.image(image_path6, caption='Your Image Caption', use_column_width=True)
 
 # Create a selectbox to choose from the available images
@@ -70,11 +70,11 @@ else:
 
 # Dictionary mapping attack names to their feature importance images
 image_paths1 = {
-    'Neptune': '/Users/disha/Desktop/networks/FIneptune.png',
-    'Satan': '/Users/disha/Desktop/networks/FISatan.png',
-    'Smurf': '/Users/disha/Desktop/networks/FIsmurf.png',
-    'IPSweep': '/Users/disha/Desktop/networks/IPSweep.png',
-    'PortSweep': '/Users/disha/Desktop/networks/[ortSweep].png'
+    'Neptune': 'networks/FIneptune.png',
+    'Satan': 'networks/FISatan.png',
+    'Smurf': 'networks/FIsmurf.png',
+    'IPSweep': 'networks/IPSweep.png',
+    'PortSweep': 'networks/[ortSweep].png'
 }
 
 # Create a selectbox to choose from the available attacks
